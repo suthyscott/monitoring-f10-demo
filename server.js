1d7796405f16585551dbcab8b001dd772d873f6d
suthyscott <sutherlandscott21@gmail.com>
2021-10-26 12:37:56 -0600

    added dymanic error handling

--- server.js
+++ server.js
@@ -18,15 +18,29 @@ app.get('/', (req, res)=> {
     rollbar.info('html file served successfully')
 })
 
+app.get('/style', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/styles.css'))
+})
+
 let students = []
 
 app.post('/api/student', (req, res) => {
     let {name} = req.body
     name = name.trim()
     
-    students.push(name)
-    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
-    res.status(200).send(students)
+    const index = students.findIndex(studentName => studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if( name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('Must provide a name.')
+    } else {
+        rollbar.critical('Student already exist')
+        res.status(400).send('that student already exists')
+    }
 })
 
 app.use(rollbar.errorHandler())
