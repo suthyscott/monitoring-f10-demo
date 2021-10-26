56764788a0ea0737fbc652abf74ac9d371d50037
suthyscott <sutherlandscott21@gmail.com>
2021-10-26 12:16:23 -0600

    changed constant variable

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res)=> {
 let students = []
 
 app.post('/api/student', (req, res) => {
-    const {name} = req.body
+    let {name} = req.body
     name = name.trim()
     
     students.push(name)
