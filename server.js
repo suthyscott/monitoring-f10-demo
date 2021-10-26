aeb67507bd56b002d311ca09010e5484c9ca41d1
suthyscott <sutherlandscott21@gmail.com>
2021-10-26 12:02:51 -0600

    added rollbar errorhandler

--- server.js
+++ server.js
@@ -11,10 +11,24 @@ let rollbar = new Rollbar({
 const app = express()
 
 // console.log(__dirname, path)
+
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
 })
+
+let students = []
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+    
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
